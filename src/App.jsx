35def736280274ce2b24487fd7c20fbd7e52e33b
7337475780 <tharunlingala6@gmail.com>
2025-05-18 19:31:09 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import ProductCards from './components/ProductCards';
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProductCards from "./components/ProductCards";
 
 function App() {
-  
-
   return (
     <>
       <Navbar />
       <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
